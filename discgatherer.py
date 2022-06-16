34efa523c906b43e9660bc60313c9a9e5adeb17a
Arkadiusz Bulski <arek.bulski@gmail.com>
2022-06-16 08:46:39 +0200

    WIP Interrogating the /dev/sr0 and corresponding filesystem.

--- discgatherer.py
+++ discgatherer.py
@@ -1,10 +1,10 @@
-# This is the DiscGatherer software. Its purpose is to help you manage your collection of CD DVD and Bluray dics. If the CLI interface is not to your liking, your collection may be also exposed through FUSE. Stay tuned.
+# This is the DiscGatherer software. Its purpose is to help you manage your collection of CD DVD and Bluray dics. Stay tuned.
 
 # For a start, there are a few dependencies. Use `pip install xxx`.
 import bson
 
 # Following are standard library imports. They should not fail, ever.
-import os, argparse
+import os, argparse, subprocess, pprint
 
 # By default, the collection is stored in `default.db` but you could change that if you want. If the database file does not exist, the program just assumes that the database is empty.
 collection = {}
@@ -16,12 +16,76 @@ if os.path.exists(databasename):
 		serializeddata = f.read()
 	collection = bson.loads(serializeddata)
 
-# The dafualt way of using this program is through the CLI commandline. Even if there is a way of exposing the collection through FUSE, adding new discs to your collection still requires you to use CLI syntax. Learn it.
-description = "This is the DiscGatherer software. Its purpose is to help you manage your collection of CD DVD and Bluray dics. The dafualt way of using this program is through the CLI commandline. Even if there is a way of exposing the collection through FUSE, adding new discs to your collection still requires you to use CLI syntax. Learn it."
+description = "This is the DiscGatherer software. Its purpose is to help you manage your collection of CD DVD and Bluray dics. The default way of using this program is through the CLI command-line. This covers both listing your discs, adding them, searching for specific files, etc. Most operations can only be done using CLI syntax. Learn it."
 parser = argparse.ArgumentParser(description=description, add_help=False)
 parser.add_argument("-h", "--help", action="help", help="Display documentation.")
 parser.add_argument("-a", "--add", action="store_true", help="Add /dev/sr0 disc to your collection.")
+parser.add_argument("-v", "--verbose", action="store_true", help="Print debug information on every step.")
 args = parser.parse_args()
+
+if args.add:
+	# First thing: lets interrogate `udevadm info` about the `/dev/sr0` disc. This command interrogates both the optical drive as well as the optical disc inside of it. It returns a lot of data about both, that needs to be parsed and sifted through.
+	sp = subprocess.run(["udevadm","info","-q","property","-x","-n","sr0"], stdout=subprocess.PIPE)
+	output = sp.stdout.decode()
+	outputlines = output.splitlines()
+	def parse(line):
+		parts = line.partition("=")
+		return (parts[0], parts[2].strip("'"))
+	def select(kv):
+		(k,v) = kv
+		return k in ["DEVNAME", "DEVTYPE", "ID_CDROM_MEDIA_BD", "ID_CDROM_MEDIA_STATE", "ID_CDROM_MEDIA_SESSION_COUNT", "ID_FS_LABEL", "ID_FS_LABEL_ENC", "ID_FS_TYPE", "ID_FS_VERSION", "ID_FS_USAGE", "ID_FS_UUID", "ID_FS_UUID_ENC", "ID_FS_VOLUME_SET_ID", "ID_FS_VOLUME_ID", "ID_FS_LOGICAL_VOLUME_ID", "ID_FS_APPLICATION_ID", "ID_FS_BOOT_SYSTEM_ID"]
+	items = {k:v for (k,v) in map(parse, outputlines)}
+	if args.verbose:
+		print("Interrogating the disc drive yielded following (ALL ENTRIES): ")
+		pprint.pprint(items)
+		print()
+	items = {k:v for (k,v) in filter(select, map(parse, outputlines))}
+	if args.verbose:
+		print("Interrogating the disc drive yielded following (FILTERED): ")
+		pprint.pprint(items)
+		print()
+	if len(items) <= 2:
+		print("There seems to be no disc present, aborting.")
+		print()
+		exit(1)
+	# TODO: Allow overriding the disc label with an explicit one.
+	# TODO: Is this the correct value for a label?
+	disclabel = items.get("ID_FS_LOGICAL_VOLUME_ID", None) or items.get("ID_FS_VOLUME_SET_ID", None) or items.get("ID_FS_LABEL_ENC", None) or "[unknown label]"
+	disclabel = disclabel.encode().decode("unicode-escape")
+	if args.verbose:
+		print("The label used for the disc: ")
+		print(repr(disclabel))
+		print()
+
+	# Second thing: walk over the corresponding mounted filesystem. The entire directory tree rooted at the disc filesystem is cataloged, every file and folder stat-ed and recorded in the database. 
+	fsname = items["ID_FS_LABEL_ENC"]
+	fsname = fsname.encode().decode("unicode-escape")
+	fspath = f"/media/{os.getlogin()}/{fsname}/"
+	if args.verbose:
+		print("Path up to the mountpoint should be: ")
+		print(repr(fspath))
+		print()
+		print("Files and folders at root: ")
+		for fn in os.listdir(fspath):
+			print(repr(fn))
+		print()
+		print("All files and folders: ")
+		for (root,dirs,files) in os.walk(fspath, topdown=False):
+			print(f"in {repr(root)} there is:")
+			for d in dirs:
+				print(f"  a folder {repr(d)}")
+			for f in files:
+				print(f"  a file {repr(f)}")
+		print()
+	
+# TODO: What to do when there is more than one optical drive or disc?
+# Possible answer: Maybe just ignore the other drives and use the first?
+# TODO: What does happen when there is no disc in the optical drive?
+# Already handled. It just aborts.
+# TODO: What does happen when there is no optical drive at all?
+# Needs testing.
+# TODO: What does happen when you try to add Audio CD instead?
+# Needs testing.
 
 # TODO: For now the entire database is auto-saved. It should only save if changed, and only using atomic file replacement. Stay tuned.
 if autosave:
