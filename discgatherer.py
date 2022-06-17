bdd74d1996e3645fd68850f780277bab7b363cb2
Arkadiusz Bulski <arek.bulski@gmail.com>
2022-06-17 13:37:19 +0200

    Refactored. Adding mode also uses the walking method (not pprint anymore).

--- discgatherer.py
+++ discgatherer.py
@@ -34,6 +34,51 @@ parser.add_argument("-v", "--verbose", action="store_true", help="Print addition
 args = parser.parse_args()
 
 #-------------------------------------------------------------------------------
+# Common functions that are used by more than one mode.
+
+# This function converts file sizes into a human readable format like "123.4 KB"
+def formatsize(size):
+	magnitude = 0
+	suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+	for s in suffixes:
+		if size > 1000:
+			size /= 1000
+			magnitude += 1
+		else:
+			break
+	if magnitude == 0:
+		return f"{size} bytes"
+	else:
+		return f"{size:.1f} {suffixes[magnitude]}"
+
+# This function computes the indentation, four spaces per level.
+def indent(level):
+	return " " * 4 * level
+
+# This function prints out folders, recursively.
+def walk_print(entries, indentlevel):
+	# Displays sub-files before sub-folders.
+	for (entryname,entry) in entries.items():
+		if entry["type"] == "file":
+			if args.verbose:
+				size = formatsize(entry["size"])
+				mtime = datetime.datetime.utcfromtimestamp(entry["mtime"]).isoformat(sep=" ")
+				print(f"{indent(indentlevel)}a file {entryname} [size: {size}] [modified: {mtime}]")
+			else:
+				print(f"{indent(indentlevel)}a file {entryname}")
+	# Displays sub-files before sub-folders.
+	for (entryname,entry) in entries.items():
+		if entry["type"] == "folder":
+			if args.verbose:
+				size = formatsize(entry["size"])
+				print(f"{indent(indentlevel)}a folder {entryname} [size: {size}]:")
+			else:
+				print(f"{indent(indentlevel)}a folder {entryname}:")
+			walk_print(entry["entries"], indentlevel+1)
+
+
+
+#-------------------------------------------------------------------------------
 # Adding a disc mode.
 
 if args.add:
@@ -47,11 +92,13 @@ if args.add:
 	def select(kv):
 		(k,v) = kv
 		return k in ["DEVNAME", "DEVTYPE", "ID_CDROM_MEDIA_BD", "ID_CDROM_MEDIA_STATE", "ID_CDROM_MEDIA_SESSION_COUNT", "ID_FS_LABEL", "ID_FS_LABEL_ENC", "ID_FS_TYPE", "ID_FS_VERSION", "ID_FS_USAGE", "ID_FS_UUID", "ID_FS_UUID_ENC", "ID_FS_VOLUME_SET_ID", "ID_FS_VOLUME_ID", "ID_FS_LOGICAL_VOLUME_ID", "ID_FS_APPLICATION_ID", "ID_FS_BOOT_SYSTEM_ID"]
+	# TODO: Remove this later on.
 	items = {k:v for (k,v) in map(parse, outputlines)}
 	if args.verbose:
 		print("Interrogating the disc drive yielded following (ALL ENTRIES): ")
 		pprint.pprint(items)
 		print()
+	# 
 	items = {k:v for (k,v) in filter(select, map(parse, outputlines))}
 	if args.verbose:
 		print("Interrogating the disc drive yielded following (among others): ")
@@ -81,7 +128,7 @@ if args.add:
 		print(repr(fspath))
 		print()
 
-	def walk(path):
+	def walk_adding(path):
 		entries = {}
 		for entryname in os.listdir(path):
 			pathname = os.path.join(path, entryname)
@@ -89,18 +136,17 @@ if args.add:
 			if stat.S_ISREG(entrystat.st_mode):
 				entries[entryname] = dict(type="file", size=entrystat.st_size, atime=entrystat.st_atime, mtime=entrystat.st_mtime, ctime=entrystat.st_ctime)
 			if stat.S_ISDIR(entrystat.st_mode):
-				branch = walk(pathname)
+				branch = walk_adding(pathname)
 				size = sum(f["size"] for f in branch.values())
 				entries[entryname] = dict(type="folder", size=size, entries=branch, atime=entrystat.st_atime, mtime=entrystat.st_mtime, ctime=entrystat.st_ctime)
 		return entries
 
-	tree = walk(fspath)
+	tree = walk_adding(fspath)
 	size = sum(f["size"] for f in tree.values())
 	disc = dict(type="disc", label=disclabel, items=items, content=tree, size=size)
 	if args.verbose:
 		print("The disc content was found as follows: ")
-		# TODO: Make a better display for the tree. pprint is sucky.
-		pprint.pprint(disc)
+		walk_print(disc["content"], 1)
 		print()
 
 	nextid = 1 if len(collection)==0 else max(map(int, collection.keys()))+1
@@ -116,56 +162,16 @@ if args.add:
 # Listing discs mode.
 
 if args.list:
-	# This function converts file sizes into a human readable format like "123.4 KB"
-	def formatsize(size):
-		magnitude = 0
-		suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-		for s in suffixes:
-			if size > 1000:
-				size /= 1000
-				magnitude += 1
-			else:
-				break
-		if magnitude == 0:
-			return f"{size} bytes"
-		else:
-			return f"{size:.1f} {suffixes[magnitude]}"
-
-	# This function computes the indentation, four spaces per level.
-	def indent(level):
-		return " " * 4 * level
-
-	# This function prints out folders, recursively.
-	def walk(entries, indentlevel):
-		# Displays sub-files before sub-folders.
-		for (entryname,entry) in entries.items():
-			if entry["type"] == "file":
-				if args.verbose:
-					size = formatsize(entry["size"])
-					mtime = datetime.datetime.utcfromtimestamp(entry["mtime"]).isoformat(sep=" ")
-					print(f"{indent(indentlevel)}a file {entryname} [size: {size}] [modified: {mtime}]")
-				else:
-					print(f"{indent(indentlevel)}a file {entryname}")
-		# Displays sub-files before sub-folders.
-		for (entryname,entry) in entries.items():
-			if entry["type"] == "folder":
-				if args.verbose:
-					size = formatsize(entry["size"])
-					print(f"{indent(indentlevel)}a folder {entryname} [size: {size}]:")
-				else:
-					print(f"{indent(indentlevel)}a folder {entryname}:")
-				walk(entry["entries"], indentlevel+1)
-
 	# Displays all discs, recursively.
 	for (entryid,entry) in collection.items():
 		if args.verbose:
 			size = formatsize(entry["size"])
 			print(f"[{entryid}] {entry['type']} {entry['label']} [size: {size}]:")
-			walk(entry["content"], 1)
+			walk_print(entry["content"], 1)
 			print()
 		else:
 			print(f"[{entryid}] {entry['type']} {entry['label']}")
-			walk(entry["content"], 1)
+			walk_print(entry["content"], 1)
 			print()
 
 # TODO: How about using colorama to display names in bold?
